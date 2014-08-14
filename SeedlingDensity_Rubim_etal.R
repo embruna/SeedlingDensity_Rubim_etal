e1f9354e1b0154e2fe39bc458d889ee35a03ca09
embruna <embruna@ufl.edu>
2014-08-13 22:40:03 -0400

    added a line at top to see if commits working

--- SeedlingDensity_Rubim_etal.R
+++ SeedlingDensity_Rubim_etal.R
@@ -25,7 +25,7 @@ summary(Exp_Data)
 ###Will need to revise this to import the raw data and calclulate RGR from Raw data
 ###Forst do calclualtions, then join into one file, then do ANOVA
 ###this is a test to see if SSH worked
-####This is to test if working for Ethan...and now testing again...and again.
+####Deleted this line
 #################
 ###CALCLULATIONS OF RGR BAsed on Height & Leaf Area
 ##################
