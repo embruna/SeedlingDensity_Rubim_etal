a120639490bf73f6eae97634e4cdbc0eb37a8555
embruna <embruna@ufl.edu>
2014-08-18 20:47:59 -0400

    Added some code to convert from wide to long

--- SeedlingDensity_Rubim_etal.R
+++ SeedlingDensity_Rubim_etal.R
@@ -8,6 +8,7 @@
 setwd("/Users/emiliobruna/Dropbox/Rubim Manuscripts/Seedling density[3]/EB Revision")
 library(gdata)
 library(ggplot2)
+library(reshape)
 
 
 
@@ -19,12 +20,33 @@ library(ggplot2)
 rm(list=ls())
 
 #load the  CSV files and save them as dataframes
-Exp_Data<-read.csv("Seedling_Experimental_Data_11august2014.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
+Exp_Data<-read.csv("cohort2-leaf_length.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
+Exp_Data_ht<-read.csv("cohort2-ht.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
+
 #look at the summary - all look ok?
 summary(Exp_Data)
 
-###Will need to revise this to import the raw data and calclulate RGR from Raw data
-###Forst do calclualtions, then join into one file, then do ANOVA
+
+#step 2: reshape data - wide to long
+
+mdata <- melt(Exp_Data, id=c("block","trt", "sdlg.no", "sdlg.type"))
+mdata2 <- mdata[order(mdata$block,mdata$trt, mdata$sdlg.no, mdata$sdlg.type),]
+mdata3 <- na.omit(mdata2)
+
+mdata.ht <- melt(Exp_Data_ht, id=c("block","trt", "sdlg.no", "sdlg.type"))
+mdata2.ht <- mdata.ht[order(mdata.ht$block,mdata.ht$trt, mdata.ht$sdlg.no, mdata.ht$sdlg.type),]
+mdata3.ht <- na.omit(mdata2.ht)
+
+
+write.csv(mdata3, "cohort2_long")
+
+#step 3: calclulate leaf area for each leaf
+#calclualte leaf area missing from each leaf
+#subtract missing leaf area to get final leaf area
+#sum to get total plant leaf area in each itme period
+#calclulate realtive growth rate t0 vs tfinal for each cohort
+#
+#
 
 #################
 ###CALCLULATIONS OF RGR BAsed on Height & Leaf Area
