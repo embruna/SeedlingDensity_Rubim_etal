e439fbea068f0cd65c6239c90bad8a213ee128a2
embruna <embruna@ufl.edu>
2014-08-23 00:51:03 -0400

    Finished cohort 2 code, added code to aggregate by category

--- SeedlingDensity_Rubim_etal.R
+++ SeedlingDensity_Rubim_etal.R
@@ -18,7 +18,7 @@ rm(list=ls())
 #load the  CSV files and save them as dataframes
 Exp_Data_C2<-read.csv("EXP_DATA_COHORT2_18august2014.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
 summary(Exp_Data_C2)
-Exp_Data_C1<-read.csv("EXP_DATA_COHORT1_18august2014.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
+Exp_Data_C1<-read.csv("EXP_DATA_COHORT1_23august2014.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
 summary(Exp_Data_C1)
 
 #####NOTE THAT STEP 2 and STEP 3 are essentially the same thing, should be made into a function!!!!
@@ -29,10 +29,10 @@ summary(Exp_Data_C1)
 ##################
 
 #select the columns for leaf length 
-mdata.la<-Exp_Data_C2[1:33]
+mdata.la<-Exp_Data_C2[1:34]
 #Now melt them with reshape2 package to create a dataframe with 
 #leaf-lengths in long form 
-mdata.la <- melt(mdata.la, id.var=c("seedling.id.no","block","trt", "sdlg.no", "sdlg.type"))
+mdata.la <- melt(mdata.la, id.var=c("cohort","seedling.id.no","block","trt", "sdlg.no", "sdlg.type"))
 #rename the column "value" as leaf length
 names(mdata.la)[names(mdata.la)=="value"] <- "leaf.length"
 #now split the column with leaf number and time interval into 
@@ -45,7 +45,7 @@ mdata.la<-cbind(mdata.la,split)
 mdata.la$leaf <- NULL
 #now add a column with % of leaf area missing to that dataframe
 #to do so create a dataframe with leaf-areas missing in long form
-mdata.miss<-Exp_Data_C2[,c(1,34:61)]
+mdata.miss<-Exp_Data_C2[,c(2,35:62)]
 mdata.miss <- melt(mdata.miss, id.var=c("seedling.id.no"))
 #then rename the column "value" for as "leaf.percentage.missing"
 names(mdata.miss)[names(mdata.miss)=="value"] <- "leaf.percentage.missing"
@@ -54,9 +54,6 @@ cohort2.la<-cbind(mdata.la,mdata.miss$leaf.percentage.missing)
 names(cohort2.la)[names(cohort2.la)=="mdata.miss$leaf.percentage.missing"] <- "leaf.percentage.missing"
 #the final steps are cleanup: remove NA, which leaves for each plant with records for only the leaves it actually had
 cohort2.la<-na.omit(cohort2.la)
-#and to add a column identifying which cohort this is (which you will need when you rbind cohort 1 and 2 for analyses)
-cohort2.la[, "cohort"] <- "Cohort 2"
-
 
 ##################
 ###step 3: calclulate the area of each leaf, subtract the area missing from each leaf
@@ -72,7 +69,7 @@ cohort2.la[, "corrected.leaf.area"] <-cohort2.la$uncorrected.leaf.area - (cohort
 ###step 4: DO EXACTLY THE SAME FOR COHORT 1
 ##################
 #load the  CSV files and save them as dataframes
-Exp_Data_C1<-read.csv("EXP_DATA_COHORT1_18august2014.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
+Exp_Data_C1<-read.csv("EXP_DATA_COHORT1_23august2014.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
 summary(Exp_Data_C1)
 
 ##################
@@ -81,10 +78,10 @@ summary(Exp_Data_C1)
 ##################
 
 #select the columns for leaf length 
-mdata.la.c1<-Exp_Data_C1[1:33]
+mdata.la.c1<-Exp_Data_C1[1:82] 
 #Now melt them with reshape2 package to create a dataframe with 
 #leaf-lengths in long form 
-mdata.la.c1 <- melt(mdata.la.c1, id.var=c("seedling.id.no","block","trt", "sdlg.no", "sdlg.type"))
+mdata.la.c1 <- melt(mdata.la.c1, id.var=c("cohort", "seedling.id.no","block","trt", "sdlg.no", "sdlg.type"))
 #rename the column "value" as leaf length
 names(mdata.la.c1)[names(mdata.la.c1)=="value"] <- "leaf.length"
 #now split the column with leaf number and time interval into 
@@ -97,17 +94,16 @@ mdata.la.c1<-cbind(mdata.la.c1,split.c1)
 mdata.la.c1$leaf <- NULL
 #now add a column with % of leaf area missing to that dataframe
 #to do so create a dataframe with leaf-areas missing in long form
-mdata.miss.c1<-Exp_Data_C1[,c(1,34:61)]
+mdata.miss.c1<-Exp_Data_C1[,c(2,83:158)]  
 mdata.miss.c1 <- melt(mdata.miss.c1, id.var=c("seedling.id.no"))
 #then rename the column "value" for as "leaf.percentage.missing"
 names(mdata.miss.c1)[names(mdata.miss.c1)=="value"] <- "leaf.percentage.missing"
 #add that column to the dataframe of leaf lengths and rename that column
 cohort1.la<-cbind(mdata.la.c1,mdata.miss.c1$leaf.percentage.missing)
-names(cohort1.la)[names(cohort1.la)=="mdata.miss$leaf.percentage.missing"] <- "leaf.percentage.missing"
+names(cohort1.la)[names(cohort1.la)=="mdata.miss.c1$leaf.percentage.missing"] <- "leaf.percentage.missing"
 #the final steps are cleanup: remove NA, which leaves for each plant with records for only the leaves it actually had
 cohort1.la<-na.omit(cohort1.la)
 #and to add a column identifying which cohort this is (which you will need when you rbind cohort 1 and 2 for analyses)
-cohort1.la[, "cohort"] <- "Cohort 1"
 
 
 ##################
@@ -125,10 +121,9 @@ cohort1.la[, "corrected.leaf.area"] <-cohort1.la$uncorrected.leaf.area - (cohort
 ##################
 
 
-
-
-
-
+aggdata<-aggregate(cohort1.la$corrected.leaf.area, by=list(cohort1.la$trt,cohort1.la$sdlg.type, cohort1.la$interval), 
+                    FUN=sum, na.rm=TRUE)
+aggdata
 #################
 ###CALCLULATIONS OF RGR BAsed on Height & Leaf Area
 ##################
