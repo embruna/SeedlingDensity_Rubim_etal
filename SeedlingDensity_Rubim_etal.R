38d20640fb05d17019e38e5d200c6b36465977f0
embruna <embruna@ufl.edu>
2014-08-26 21:50:01 -0400

    1) Dealt with the failure to calclulate final corrected lead areas due to NA's in column of % missing
    2) sums leaf area for each plant in each interval; one dataframe for each experiemnt.

--- SeedlingDensity_Rubim_etal.R
+++ SeedlingDensity_Rubim_etal.R
@@ -18,8 +18,7 @@ rm(list=ls())
 #load the  CSV files and save them as dataframes
 Exp_Data_C2<-read.csv("EXP_DATA_COHORT2_18august2014.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
 summary(Exp_Data_C2)
-Exp_Data_C1<-read.csv("EXP_DATA_COHORT1_23august2014.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
-summary(Exp_Data_C1)
+
 
 #####NOTE THAT STEP 2 and STEP 3 are essentially the same thing, should be made into a function!!!!
 
@@ -53,7 +52,7 @@ names(mdata.miss)[names(mdata.miss)=="value"] <- "leaf.percentage.missing"
 cohort2.la<-cbind(mdata.la,mdata.miss$leaf.percentage.missing)
 names(cohort2.la)[names(cohort2.la)=="mdata.miss$leaf.percentage.missing"] <- "leaf.percentage.missing"
 #the final steps are cleanup: remove NA, which leaves for each plant with records for only the leaves it actually had
-cohort2.la<-na.omit(cohort2.la)
+#cohort2.la<-na.omit(cohort2.la)
 
 ##################
 ###step 3: calclulate the area of each leaf, subtract the area missing from each leaf
@@ -61,12 +60,15 @@ cohort2.la<-na.omit(cohort2.la)
 ##################
 #Adds a column with the leaf area (uncorrected). LA is calculated using the formula in Bruna 2002 Oecologia 
 cohort2.la[, "uncorrected.leaf.area"] <- 0.53+(0.831*cohort2.la$leaf.length)
-#adds a column with the "corrected" leaf area (i.e., corrected leaf area - % missing) 
+
+#first removes NA from the column of percet of each leaf missing (in some cases NA because there was no measurment was taken
+#then adds a column with the "corrected" leaf area (i.e., corrected leaf area - % missing) 
+cohort2.la$leaf.percentage.missing[is.na(cohort2.la$leaf.percentage.missing)] <- 0
 cohort2.la[, "corrected.leaf.area"] <-cohort2.la$uncorrected.leaf.area - (cohort2.la$uncorrected.leaf.area*(cohort2.la$leaf.percentage.missing/100))
 
 
 ##################
-###step 4: DO EXACTLY THE SAME FOR COHORT 1
+###DO EXACTLY THE SAME FOR COHORT 1
 ##################
 #load the  CSV files and save them as dataframes
 Exp_Data_C1<-read.csv("EXP_DATA_COHORT1_23august2014.csv", dec=".", header = TRUE, sep = ",", check.names=FALSE )
@@ -102,9 +104,7 @@ names(mdata.miss.c1)[names(mdata.miss.c1)=="value"] <- "leaf.percentage.missing"
 cohort1.la<-cbind(mdata.la.c1,mdata.miss.c1$leaf.percentage.missing)
 names(cohort1.la)[names(cohort1.la)=="mdata.miss.c1$leaf.percentage.missing"] <- "leaf.percentage.missing"
 #the final steps are cleanup: remove NA, which leaves for each plant with records for only the leaves it actually had
-cohort1.la<-na.omit(cohort1.la)
 #and to add a column identifying which cohort this is (which you will need when you rbind cohort 1 and 2 for analyses)
-
 
 ##################
 ###step 3: calclulate the area of each leaf, subtract the area missing from each leaf
@@ -112,18 +112,47 @@ cohort1.la<-na.omit(cohort1.la)
 ##################
 #Adds a column with the leaf area (uncorrected). LA is calculated using the formula in Bruna 2002 Oecologia 
 cohort1.la[, "uncorrected.leaf.area"] <- 0.53+(0.831*cohort1.la$leaf.length)
-#adds a column with the "corrected" leaf area (i.e., corrected leaf area - % missing) 
+#first removes NA from the column of percet of each leaf missing (in some cases NA because there was no measurment was taken
+#then adds a column with the "corrected" leaf area (i.e., corrected leaf area - % missing) 
+cohort1.la$leaf.percentage.missing[is.na(cohort1.la$leaf.percentage.missing)] <- 0
 cohort1.la[, "corrected.leaf.area"] <-cohort1.la$uncorrected.leaf.area - (cohort1.la$uncorrected.leaf.area*(cohort1.la$leaf.percentage.missing/100))
 
-
-##################
-###step 4: ROW BIND THE TWO, then do some summarizing of leaf area for each plant to calclulate the RGR
+###step 4: AGGREGATE DATA AS NEEDED FOR ANALYSIS  
 ##################
 
 
-aggdata<-aggregate(cohort1.la$corrected.leaf.area, by=list(cohort1.la$trt,cohort1.la$sdlg.type, cohort1.la$interval), 
+###Q: shoudl each cohort get its own anova?  Cohort lengths were different and included multiple years, so probably not a good idea to combine in sinngle analysis?
+
+#Aggregate data: summed the leaf-areas for each plant in each sampling interval
+COHORT1<-aggregate(cohort1.la$corrected.leaf.area, by=list(cohort1.la$cohort, cohort1.la$block, 
+                    cohort1.la$trt, cohort1.la$sdlg.type,cohort1.la$seedling.id.no, cohort1.la$interval), 
                     FUN=sum, na.rm=TRUE)
-aggdata
+#rename the columns
+names(COHORT1)[1] <- "cohort"
+names(COHORT1)[2] <- "block"
+names(COHORT1)[3] <- "trt"
+names(COHORT1)[4] <- "sdlg.type"
+names(COHORT1)[5] <- "sdlg.id.no"
+names(COHORT1)[6] <- "interval"
+names(COHORT1)[7] <- "leaf.area"
+
+COHORT1
+
+
+COHORT2<-aggregate(cohort2.la$corrected.leaf.area, by=list(cohort2.la$cohort, cohort2.la$block, 
+                    cohort2.la$trt, cohort2.la$sdlg.type,cohort2.la$seedling.id.no, cohort2.la$interval), 
+                    FUN=sum, na.rm=TRUE)
+names(COHORT2)[1] <- "cohort"
+names(COHORT2)[2] <- "block"
+names(COHORT2)[3] <- "trt"
+names(COHORT2)[4] <- "sdlg.type"
+names(COHORT2)[5] <- "sdlg.id.no"
+names(COHORT2)[6] <- "interval"
+names(COHORT2)[7] <- "leaf.area"
+COHORT2
+
+
+
 #################
 ###CALCLULATIONS OF RGR BAsed on Height & Leaf Area
 ##################
